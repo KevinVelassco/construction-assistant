641b439e989b60b5a7c030afa75d04d4e5088e1a
KevinVelassco <kevinvelassco@gmail.com>
2021-10-17 17:55:42 -0500

    feat: method responsible for validating verification codes is created

--- src/services/verification-code.service.ts
+++ src/services/verification-code.service.ts
@@ -1,7 +1,9 @@
 import { getRepository } from 'typeorm';
+import { HttpException } from '../common/http-exception';
 import { VerificationCode } from '../entities/verification-code.entity';
 import { generateUuid } from '../utils/generateUuid';
 import { CreateVerificationCodeInput } from '../dto/verification-codes/create-verification-code-input.dto';
+import { ValidateVerificationCodeInput } from '../dto/verification-codes/validate-verification-code-input.dto';
 
 export class VerificationCodeService {
   static async create(
@@ -17,5 +19,46 @@ export class VerificationCodeService {
     const saved = await verificationCodeRepository.save(created);
 
     return saved;
+  }
+
+  static async validate(
+    validateVerificationCodeInput: ValidateVerificationCodeInput
+  ): Promise<VerificationCode> {
+    const { code } = validateVerificationCodeInput;
+
+    const verificationCodeRepository = getRepository(VerificationCode);
+
+    const existing = await verificationCodeRepository.findOne({
+      where: {
+        code
+      },
+      relations: ['user']
+    });
+
+    if (!existing) {
+      throw new HttpException(
+        404,
+        `the code ${code} is invalid, probably because it was already used.`
+      );
+    }
+
+    const currentDate = new Date();
+
+    const { expirationDate } = existing;
+
+    if (currentDate.getTime() > expirationDate.getTime()) {
+      throw new HttpException(409, `the verification code ${code} is expired.`);
+    }
+
+    const { type } = validateVerificationCodeInput;
+
+    if (type !== existing.type) {
+      throw new HttpException(
+        409,
+        `the verification code ${code} doesn't have the expected type.`
+      );
+    }
+
+    return existing;
   }
 }
