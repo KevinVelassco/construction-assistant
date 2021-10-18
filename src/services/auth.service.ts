d4be987fdd08043e1ab80d16a5ad23f7520a4df4
KevinVelassco <kevinvelassco@gmail.com>
2021-10-17 19:52:24 -0500

    feat: method responsible for sending password update email is created

--- src/services/auth.service.ts
+++ src/services/auth.service.ts
@@ -6,6 +6,10 @@ import { User } from '../entities/user.entity';
 import { ChangeAuthPasswordInput } from '../dto/auths/change-auth-password-input.dto';
 import { UserService } from './user.service';
 import { RefreshAuthTokenInput } from '../dto/auths/refresh-auth-token-input.dto';
+import { TemplateService } from '../templates/template.service';
+import { MailgunService } from '../plugins/mailgun/mailgun.service';
+import { ParameterService } from './parameter.service';
+import { SendAuthPasswordUpdateEmailInput } from '../dto/auths/send-auth-password-update-email-input.dto';
 
 export class AuthService {
   static async login(loginAuthInput: LoginAuthInput): Promise<Object> {
@@ -119,5 +123,47 @@ export class AuthService {
     } catch (e) {
       throw new HttpException(409, e.message);
     }
+  }
+
+  private static async sendPasswordUpdateEmail(
+    sendAuthPasswordUpdateEmailInput: SendAuthPasswordUpdateEmailInput
+  ) {
+    const { authUid, email } = sendAuthPasswordUpdateEmailInput;
+
+    const existing = await UserService.getUserByAuthUidAndEmail({
+      authUid,
+      email
+    });
+
+    if (!existing) {
+      throw new HttpException(
+        404,
+        `can't get the user with email ${email} for the authUid ${authUid}`
+      );
+    }
+
+    const html = TemplateService.generateHtmlByTemplate(
+      'updated-password-notification',
+      {
+        email,
+        name: existing.name,
+        link: `${process.env.SELF_WEB_URL}recover-password`
+      }
+    );
+
+    const from = <string>process.env.MAILGUN_EMAIL_FROM;
+
+    const parameterName = 'UPDATED_PASSWORD_EMAIL_SUBJECT';
+
+    const subject = await ParameterService.getParameterValue({
+      name: parameterName
+    });
+
+    await MailgunService.sendEmail({
+      from,
+      to: email,
+      subject: <string>subject,
+      html
+    });
   }
 }
